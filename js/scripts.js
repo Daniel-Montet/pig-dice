8e618ab63f57cc3439ea7686c6a313b407244c81
Daniel Montet <dmontetproff@gmail.com>
2019-03-02 22:53:18 +0300

    Add Player and Switch classes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,37 @@
-//var player1=prompt("Enter player 1 name")
-//var player2=prompt("Enter player 2 name")
+class Player{
+    constructor(name){
+        this.name=name
+    }
+//INSTANCE METHODS
+    name(){
+        return this.name
+    }
+
+}
+
+class Switch extends Player{//DESCRIBE PROPERTIES OF DIFFERENT AVATARS
+    constructor(avatar1,avatar2){
+        super(name)
+        this.avatar1=avatar1
+        this.avatar2=avatar2
+    }
+    avatar1(){
+        return this.avatar1
+    }
+    avatar2(){
+        return this.avatar2
+    }   
+    
+}
+
+
 
 var maxscore=100
 var scoreboard= 0
 var currentscore=0
 var turnscore=0
-//var hold=true
-//var rollingorhold=[rollPlayer1(),rollPlayer2()]
 
-
-function rollPlayer1(){
+function rollPlayer(player){//CALCULATE ROLL FUNCTIONALITY FOR AVATARS
     turnscore=Math.floor(Math.random()*6)+1;
     if(turnscore!=1){
         currentscore+=turnscore
@@ -20,13 +42,3 @@ function rollPlayer1(){
     console.log(currentscore)
 }
 
-function rollPlayer2(){
-    turnscore=Math.floor(Math.random()*6)+1;
-    if(turnscore!=1){
-        currentscore+=turnscore
-    }
-    else{
-        currentscore-=currentscore
-    }
-    console.log(currentscore)
-}
