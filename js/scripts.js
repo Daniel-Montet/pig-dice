3ea0268b48ccb181fd853d358954ec273d3f34c2
Daniel Montet <dmontetproff@gmail.com>
2019-03-03 09:38:15 +0300

    Add a roll function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 class Player{
     constructor(name){
         this.name=name
@@ -9,7 +11,8 @@ class Player{
 
 }
 
-class Switch extends Player{//DESCRIBE PROPERTIES OF DIFFERENT AVATARS
+class Avatar extends Player{//DESCRIBE PROPERTIES OF DIFFERENT AVATARS
+
     constructor(avatar1,avatar2){
         super(name)
         this.avatar1=avatar1
@@ -20,25 +23,37 @@ class Switch extends Player{//DESCRIBE PROPERTIES OF DIFFERENT AVATARS
     }
     avatar2(){
         return this.avatar2
-    }   
-    
+    }
+
 }
 
 
 
+
+var av=new Avatar("brian","kennedy")
+var player1=(av.avatar1)
+var player2=(av.avatar2)
 var maxscore=100
 var scoreboard= 0
 var currentscore=0
 var turnscore=0
 
-function rollPlayer(player){//CALCULATE ROLL FUNCTIONALITY FOR AVATARS
+function rollPlayer(){//CALCULATE ROLL FUNCTIONALITY FOR AVATARS
     turnscore=Math.floor(Math.random()*6)+1;
     if(turnscore!=1){
         currentscore+=turnscore
+        if(currentscore===maxscore){
+            scoreboard+=currentscore
+            console.log("you win")
+            return
+        }
+       // else if(currentscore<maxscore && hold==true){
+        //    scoreboard+=currentscore
+            //RUN FUNCTION
+       // }
     }
     else{
         currentscore-=currentscore
     }
     console.log(currentscore)
 }
-
