95519c3ef4bc28953665c20ab18b2ace63c46262
Daniel Montet <dmontetproff@gmail.com>
2019-03-04 14:51:52 +0300

    Add output control functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   var name2=''
   var hold1=''
   var hold2=''
-  $('#avatar1btn').click(function(event){
+  $('#avatar1btn').click(function(){
     name1=$('#avatar1name').val();
     getname1(name1);
   })
@@ -15,11 +15,11 @@ $(document).ready(function(){
 
   $("#hold1").click(function(){
     hold1=true
-    console.log("Its players two turn")
+    $("#turn").text("Its "+name2+ "'s turn")
   })
 
   $("#avatar1roll").click(function(){
-    console.log(name1+" " + hold1)
+  //  console.log(name1+" " + hold1)
 
     rollPlayer1(name1,hold1,name2,hold2)
 
@@ -31,21 +31,25 @@ $(document).ready(function(){
 
   })
   $("#hold2").click(function(){
-    console.log("Its players one turn")
+    $("#turn").text("Its "+name1+ "'s turn")
     hold2=true
+  })
+  $("#next").click(function(){
+    $("#page2").show()
+    $("#page1").hide()
   })
 
 })
 
 function getname1(name1){//CAPTURE PLAYER ONE NAME
     player1= new Avatar1(name1)
-    $('#player1name').text("This is your corner " + player1.avatar1)
+    $('#player1name').text(player1.avatar1)
     return player1
 }
 
 function getname2(name2){///CAPTURE PLAYER 2 NAME
     var player2= new Avatar2(name2)
-    $('#player2name').text("This is your corner " + player2.avatar2)
+    $('#player2name').text(player2.avatar2)
     return player2
 }
 
@@ -67,38 +71,26 @@ class Avatar1 extends Player{//DESCRIBE PROPERTIES OF AVATAR 1
     constructor(avatar1){
         super(name)
         this.avatar1=avatar1
-  //      this.avatar2=avatar2
     }
     avatar1(){
         return this.avatar1
     }
-  //  avatar2(){
-  //      return this.avatar2
-    //}
+
 
 }
 class Avatar2 extends Player{//DESCRIBE PROPERTIES OF AVATAR 2
 
     constructor(avatar2){
         super(name)
-    //    this.avatar1=avatar1
        this.avatar2=avatar2
    }
-  //  avatar1(){
-  //      return this.avatar1
-    //}
+
     avatar2(){
         return this.avatar2
     }
 
 }
 
-
-///console.log(avatar1.avatar1())
-//console.log(avatar2.avatar2())
-//var av=new Avatar("brian","kennedy")
-//var player1=(av.avatar1)
-//var player2=(av.avatar2)
 var maxscore=100
 var scoreboard1= 0
 var currentscore1=0
@@ -107,7 +99,7 @@ var scoreboard2= 0
 var currentscore2=0
 var turnscore2=0
 
-function rollPlayer1(name1,hold1,name2,hold2){//CALCULATE ROLL FUNCTIONALITY FOR AVATARS
+function rollPlayer1(name1,hold1){//CALCULATE ROLL FUNCTIONALITY FOR AVATARS
     this.name=name1
     this.hold=false
     this.currentscore=0
@@ -115,38 +107,36 @@ function rollPlayer1(name1,hold1,name2,hold2){//CALCULATE ROLL FUNCTIONALITY FOR
     this.scoreboard=scoreboard1
 
     turnscore1=Math.floor(Math.random()*6)+1;
-    console.log("Player1 turnscore = " +turnscore1)
+    $('#player1rollscore').text("Player1 rollscore = " +turnscore1)
     if(currentscore1<maxscore && hold1===true){
         scoreboard1+=currentscore1
         currentscore1-=currentscore1
-        console.log("its players 2 turn and the currentscore is "+currentscore1)
-       // return hold1=false
-       // rollPlayer2(name2,hold2)
-       console.log("player1 scoreboard score " + this.scoreboard1)
-        
+        $('#player1currentscore').text("its players 2 turn and the current round score is "+currentscore1)
+       $('#player1scoreborad').text("player1 scoreboard score " + this.scoreboard1)
+
 
 
     }
     if(turnscore1!=1){
         currentscore1+=turnscore1
-        console.log("Player1 current round score "+currentscore1)
-             
+        $('#player1currentscore').text("Player1 current round score "+currentscore1)
+
     }
     if(turnscore1==1){
         currentscore1-=currentscore1
-        console.log("Player1 hit a 1 and current round score is "+currentscore1)
-             
-    } 
+        $('#player1currentscore').text("Player1 hit a 1 and current round score is "+currentscore1)
+
+    }
     if(scoreboard1===maxscore){////display win
-        console.log("you win")
+        $('#player1scoreborad').text("You win, you hit " + this.scoreboard1)
         return
     }
-    
-    
+
+
 }
 
 
-function rollPlayer2(name1,hold1,name2,hold2){//CALCULATE ROLL FUNCTIONALITY FOR AVATARS
+function rollPlayer2(name2,hold2){//CALCULATE ROLL FUNCTIONALITY FOR AVATARS
     this.name=name2
     this.hold=false
     this.currentscore=currentscore2
@@ -159,20 +149,20 @@ function rollPlayer2(name1,hold1,name2,hold2){//CALCULATE ROLL FUNCTIONALITY FOR
         currentscore2-=currentscore2
         console.log("its players 1 turn and the currentscore is "+currentscore2)
        console.log("player2 scoreboard score " + this.scoreboard2)
-        
+
 
 
     }
     if(turnscore2!=1){
         currentscore2+=turnscore2
         console.log("Player2 current round score "+currentscore2)
-             
+
     }
     if(turnscore2==1){
         currentscore2-=currentscore2
         console.log("Player2 hit a 1 and current round score is "+currentscore2)
-             
-    } 
+
+    }
     if(scoreboard2===maxscore){////display win
         console.log("you win")
         return
